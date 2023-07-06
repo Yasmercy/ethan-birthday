75e182d2d941af1b92b70d7e4405bb28b83f4f13
Yasmercy <ianchensoftware@gmail.com>
2023-07-05 21:27:10 -0700

    first

---
+++ tests/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vagrant/src/ethan_birthday/src/storage.cpp" "tests/CMakeFiles/test.dir/__/src/storage.cpp.o" "gcc" "tests/CMakeFiles/test.dir/__/src/storage.cpp.o.d"
+  "/home/vagrant/src/ethan_birthday/src/wordle.cpp" "tests/CMakeFiles/test.dir/__/src/wordle.cpp.o" "gcc" "tests/CMakeFiles/test.dir/__/src/wordle.cpp.o.d"
+  "/home/vagrant/src/ethan_birthday/tests/test.cpp" "tests/CMakeFiles/test.dir/test.cpp.o" "gcc" "tests/CMakeFiles/test.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tests/test.cpp
@@ -0,0 +1,71 @@
+#include "../inc/Catch2/src/catch2/catch_test_macros.hpp"
+#include "../src/wordle.h"
+
+
+TEST_CASE ("wordle-highlights no repeats", "[get_colors]") {
+    auto key = Word("venom", 5);
+    std::vector<State> sol;
+    
+    // venom --> [g, g, g, g, g]
+    auto venom = get_colors(key, Word("venom", 5));
+    sol = {GREEN, GREEN, GREEN, GREEN, GREEN};
+    REQUIRE (venom.colors == sol);
+    REQUIRE (venom.word.word == "venom");
+
+    // swear --> [r, r, y, r, r]
+    auto swear = get_colors(key, Word("swear", 5));
+    sol = {GRAY, GRAY, YELLOW, GRAY, GRAY};
+    REQUIRE (swear.colors == sol);  
+    REQUIRE (swear.word.word == "swear");
+
+    // count --> [r, y, r, y, r]
+    auto count = get_colors(key, Word("count", 5));
+    sol = {GRAY, YELLOW, GRAY, YELLOW, GRAY};
+    REQUIRE (count.colors == sol);  
+    REQUIRE (count.word.word == "count");
+}
+
+TEST_CASE ("wordle-highlights repeats", "[get_colors]") {
+    auto key = Word("venom", 5);
+    std::vector<State> sol;
+    
+    // venom --> [g, g, g, g, g]
+    auto venom = get_colors(key, Word("venom", 5));
+    sol = {GREEN, GREEN, GREEN, GREEN, GREEN};
+    REQUIRE (venom.colors == sol);
+    REQUIRE (venom.word.word == "venom");
+
+    // teeth --> [r, g, r, r, r]
+    auto teeth = get_colors(key, Word("teeth", 5));
+    sol = {GRAY, GREEN, GRAY, GRAY, GRAY};
+    REQUIRE (teeth.colors == sol);  
+    REQUIRE (teeth.word.word == "teeth");
+
+    // breed --> [r, r, y, r, r]
+    auto breed = get_colors(key, Word("breed", 5));
+    sol = {GRAY, GRAY, YELLOW, GRAY, GRAY};
+    REQUIRE (breed.colors == sol);  
+    REQUIRE (breed.word.word == "breed");
+}
+
+TEST_CASE ("wordle valid words (5 letters)", "[valid_word]") {
+    REQUIRE (valid_word(Word("venom", 5)));
+    REQUIRE (valid_word(Word("timer", 5)));
+    REQUIRE (valid_word(Word("swear", 5)));
+    REQUIRE (!valid_word(Word("VENOM", 5)));
+    REQUIRE (!valid_word(Word("abcde", 5)));
+    REQUIRE (!valid_word(Word("aaaaa", 5)));
+}
+
+
+TEST_CASE ("wordle valid words (7 letters)", "[valid_word]") {
+
+}
+
+TEST_CASE ("wordle valid words (8 letters)", "[valid_word]") {
+
+}
+
+TEST_CASE ("json for word highlight", "[to_json, from_json]") {
+
+}
